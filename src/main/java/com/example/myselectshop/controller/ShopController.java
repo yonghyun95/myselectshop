3be172c5ba3b4ce17c14b658252e735203b53578
Yonghyun Lee <dldydgus95426@gmail.com>
2023-03-22 17:37:58 +0900

    JPA 추가 기능 1. 페이징 및 정렬 구현 2. 폴더 설계 3. JPA 연관관계를 이용한 폴더 테이블 설계 4. 폴더 생성 및 조회 구현 5. 관심 상품에 폴더 추가 구현 6. 폴더별 관심 상품 조회 구현 7. 중복 폴더 생성 이슈 해결  - 같은 폴더 내에 상품 중복 생성 해결 8. EC2에 올리기(처음)

--- src/main/java/com/example/myselectshop/controller/ShopController.java
+++ src/main/java/com/example/myselectshop/controller/ShopController.java
@@ -1,12 +1,17 @@
 package com.example.myselectshop.controller;
 
 
+import com.example.myselectshop.entity.User;
+import com.example.myselectshop.jwt.JwtUtil;
+import com.example.myselectshop.repository.UserRepository;
 import com.example.myselectshop.service.FolderService;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +22,8 @@ import javax.servlet.http.HttpServletRequest;
 public class ShopController {
 
     private final FolderService folderService;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
     @GetMapping("/shop")
     public ModelAndView shop() {
@@ -24,13 +31,39 @@ public class ShopController {
     }
 
     // 로그인 한 유저가 메인페이지를 요청할 때 가지고있는 폴더를 반환
-    // 비동기 부분이며, index-Ajox 부분으로 바꿔준다
     @GetMapping("/user-folder")
     public String getUserInfo(Model model, HttpServletRequest request) {
 
         model.addAttribute("folders", folderService.getFolders(request));
 
-        return "/index :: #fragment";
+        return "index :: #fragment";
+    }
+
+    // 로그인 한 유저가 메인페이지를 요청할 때 유저의 이름 반환
+    @GetMapping("/user-info")
+    @ResponseBody
+    public String getUserName(HttpServletRequest request) {
+        String token = jwtUtil.resolveToken(request);
+        Claims claims;
+
+        if (token != null) {
+            // Token 검증
+            if (jwtUtil.validateToken(token)) {
+                // 토큰에서 사용자 정보 가져오기
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("Token Error");
+            }
+
+            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
+            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
+            );
+
+            return user.getUsername();
+        }else {
+            return "fail";
+        }
     }
 
 }
